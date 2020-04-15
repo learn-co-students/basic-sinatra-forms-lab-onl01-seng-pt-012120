7723c18b7bcaef016b496b03c3c6d595f0d1bebe
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-04-15 20:01:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -11,7 +11,13 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    @newteam = params
+    @name = params[:name]
+   @coach = params[:coach]
+   @pg = params[:pg]
+   @sg = params[:sg]
+   @pf = params[:pf]
+   @sf = params[:sf]
+   @c = params[:c]
     erb :team
   end
 
